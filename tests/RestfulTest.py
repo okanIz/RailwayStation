e134faaa6328914292f243d889e80c1eecfc7f66
okanIz <itzokidoki@gmail.com>
2021-07-13 20:37:30 +0200

    testing for statuscode & expected response

---
+++ tests/RestfulTest.py
@@ -0,0 +1,20 @@
+import requests
+import unittest
+import json
+
+URL = 'http://127.0.0.1:8080/betriebsstelle/aamp'
+response = requests.get(URL)
+response.encoding = 'UTF-8'
+
+class TestRestAPI(unittest.TestCase):
+    def testStatusCode(self):
+        assert response.status_code == 200
+
+    def testResponse(self):
+            file = open('tests/aamp.json', encoding='utf-8')
+            expectedResponses = json.load(file)
+            self.assertEqual(expectedResponses['Kurzname'], response.json()['Kurzname'])
+            file.close()
+
+if __name__ == '__main__':
+    unittest.main()
